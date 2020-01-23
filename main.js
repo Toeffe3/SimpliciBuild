dbdc2a09bdeff5514bb0957665d98b80bd7437e9
Victor Vejlgaard Schelde Jacobsen <densejevic@gmail.com>
2020-01-24 00:33:11 +0100

    Added themes and JSON-file retrieving

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-const {app, Menu, MenuItem, BrowserWindow} = require('electron')
-const path = require('path')
+const {app, Menu, MenuItem, BrowserWindow} = require('electron');
+const path = require('path');
+const fs = require('fs');
 const isMac = process.platform === 'darwin'
 const template = [
   ...(isMac ? [{
@@ -30,13 +31,14 @@ const template = [
       { type: 'separator' },
       { label: 'Settings', click: () =>  { console.log("SETTINGS") }},
       { type: 'separator' },
+      { role: 'reload' },
       isMac ? { role: 'close' } : { role: 'quit' }
     ]
   },
   {
     label: 'Edit',
     submenu: [
-      { label: 'Save', click: () =>  { console.log("SAVE") } },
+      { label: 'Save', accelerator: "CmdOrCtrl+S", click: () =>  { console.log("SAVE") } },
       { type: 'separator' },
       { role: 'undo' },
       { role: 'redo' },
@@ -45,7 +47,7 @@ const template = [
       { role: 'copy' },
       { role: 'paste' },
       { type: 'separator' },
-      { label: 'Project settings', click: () =>  { console.log("PROJECT SETTINGS") } }
+      { label: 'Project settings', accelerator: "CmdOrCtrl+,", click: () =>  { console.log("PROJECT SETTINGS") } }
     ]
   },
   {
@@ -58,7 +60,7 @@ const template = [
       { label: 'Add internationality', click: () =>  { console.log("Add internationality") } },
       { type: 'separator' },
       { label: 'Preview', click: () =>  { console.log("Preview") } },
-      { label: 'Export', click: () =>  { console.log("Export") } },
+      { label: 'Export', accelerator: "CmdOrCtrl+E", click: () =>  { console.log("Export") } },
     ]
   },
   {
@@ -89,7 +91,10 @@ let mainWindow;
 function createWindow () {
   mainWindow = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
   });
 
   mainWindow.loadFile('index.html');
